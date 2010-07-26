eab17c144de84374716dad69958073cd95d84480
Andrey M. Izrantsev <izrantsev@gmail.com>
2010-07-26 19:45:00 +0400

    Added licence information

--- exporter/exporter.c
+++ exporter/exporter.c
@@ -1,3 +1,32 @@
+/*
+
+ V-Ray/Blender
+
+ http://vray.cgdo.ru
+
+ Author: Andrey M. Izrantsev (aka bdancer)
+ E-Mail: izrantsev@gmail.com
+
+ This plugin is protected by the GNU General Public License v.2
+
+ This program is free software: you can redioutibute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is dioutibuted in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ All Rights Reserved. V-Ray(R) is a registered trademark of Chaos Group
+
+*/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -435,7 +464,11 @@ int export_scene(bContext *C, wmOperator *op)
                 mesh= get_render_mesh(sce, bmain, ob);
 
                 if(mesh) {
-                    printf("V-Ray/Blender: Exporting mesh: \033[0;32m%s\033[0m\r", ob->id.name+2);
+#ifdef _WIN32
+                    printf("V-Ray/Blender: Exporting mesh: %s                    \r", ob->id.name+2);
+#else
+                    printf("V-Ray/Blender: Exporting mesh: \033[0;32m%s\033[0m                    \r", ob->id.name+2);
+#enif
                     fflush(stdout);
 
                     write_mesh_vray(gfile, sce, ob, mesh);
--- exporter/exporter_ops.c
+++ exporter/exporter_ops.c
@@ -1,3 +1,32 @@
+/*
+
+ V-Ray/Blender
+
+ http://vray.cgdo.ru
+
+ Author: Andrey M. Izrantsev (aka bdancer)
+ E-Mail: izrantsev@gmail.com
+
+ This plugin is protected by the GNU General Public License v.2
+
+ This program is free software: you can redioutibute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is dioutibuted in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ All Rights Reserved. V-Ray(R) is a registered trademark of Chaos Group
+
+*/
+
+
 #include "BKE_main.h"
 #include "BKE_scene.h"
 #include "BKE_context.h"
@@ -18,7 +47,7 @@ void SCENE_OT_scene_export(wmOperatorType *ot)
     /* identifiers */
     ot->name= "Export scene";
     ot->idname= "SCENE_OT_scene_export";
-    ot->description="Export scene for external renderer.";
+    ot->description="Export meshes in .vrscene format.";
 
     /* api callbacks */
     ot->exec= export_scene;
@@ -26,7 +55,10 @@ void SCENE_OT_scene_export(wmOperatorType *ot)
     /* flags */
     ot->flag= 0;
 
-    RNA_def_string(ot->srna, "vray_geometry_file", "", FILE_MAX, "Geometry export file", "Geometry export file.");
+    RNA_def_string(ot->srna, "vb_geometry_file", "", FILE_MAX, "Geometry export file", "Geometry export file.");
+    RNA_def_boolean(ot->srna, "vb_active_layers", 0, "", "");
+    RNA_def_boolean(ot->srna, "vb_animation", 0, "", "");
+    //RNA_def_boolean_array(srna, "vb_render_layers", ARRAY_SIZE, NULL, "barr", "boolean array");
 }
 
 

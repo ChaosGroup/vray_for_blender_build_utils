e11391402d619244ad58485b08c73503da2973a1
Andrey M. Izrantsev <izrantsev@gmail.com>
2010-10-27 17:55:11 +0400

    fix: blender version detection

--- blender_update.py
+++ blender_update.py
@@ -537,6 +537,11 @@ else:
 	sys.stdout.write("Getting Blender sources...\n")
 	if not options.test:
 		os.system("svn checkout https://svn.blender.org/svnroot/bf-blender/trunk/blender")
+
+version_file= open(os.path.join(blender_dir,"release","VERSION"),'r')
+VERSION= version_file.read().split('-')[0]
+version_file.close()
+
 os.chdir(working_directory)
 
 

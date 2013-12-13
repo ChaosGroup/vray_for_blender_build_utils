9cb90d9198cb98fb4238a3804b0c0dcc1702f8b2
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2013-12-13 13:58:00 +0200

    fix: patch level

--- builder/builder.py
+++ builder/builder.py
@@ -320,7 +320,7 @@ class Builder:
 			if self.add_patches:
 				sys.stdout.write("Adding V-Ray/Blender patches...\n")
 
-				cmd = "%s -Np2 -i %s" % (patch_cmd, utils.path_join(patch_dir, "patch", "vray_for_blender.patch"))
+				cmd = "%s -Np1 -i %s" % (patch_cmd, utils.path_join(patch_dir, "patch", "vray_for_blender.patch"))
 
 				# Patching Blender sources
 				sys.stdout.write("Patching sources...\n")

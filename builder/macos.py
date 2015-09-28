afd130c1402c7c8522834db005481cc7bfc3ac34
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2015-09-28 15:43:47 +0300

    fix: WITH_OPENSUBDIV=ON

--- builder/macos.py
+++ builder/macos.py
@@ -59,6 +59,7 @@ class MacBuilder(Builder):
 		cmake.append("-DWITH_LIBMV=%s" % utils.GetCmakeOnOff(self.with_tracker))
 		cmake.append("-DWITH_OPENCOLLADA=%s" % utils.GetCmakeOnOff(self.with_collada))
 		cmake.append("-DWITH_MOD_OCEANSIM=ON")
+		cmake.append("-DWITH_OPENSUBDIV=ON")
 		cmake.append("-DWITH_FFTW3=ON")
 
 		cmake.append("../blender")

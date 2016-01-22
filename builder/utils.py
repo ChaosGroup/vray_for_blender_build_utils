a750a84a690efca6fd726faa9613a7b6295e28e4
Lyubomir <poseidon4o.random@gmail.com>
2016-01-22 17:02:41 +0200

    add: shortcuts file to install files

--- builder/utils.py
+++ builder/utils.py
@@ -461,6 +461,8 @@ def GenCGRInstaller(self, installer_path, InstallerDir="H:/devel/vrayblender/cgr
 	removeJunk   = set()
 	installerFiles = []
 
+	installerFiles.append('\t\t\t<FN Dest="[INSTALL_ROOT]">%s/shortcuts.py</FN>' % InstallerDir)
+
 	for dirpath, dirnames, filenames in os.walk(self.dir_install_path):
 		if dirpath.startswith('.svn') or dirpath.endswith('__pycache__'):
 			continue

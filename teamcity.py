1c40a5e0f8a48c57cfb57de200fd95928a3af26b
Lyubomir <poseidon4o.random@gmail.com>
2016-01-25 14:21:10 +0200

    add: common paths for TC

--- teamcity.py
+++ teamcity.py
@@ -58,6 +58,10 @@ def main(args):
             r'C:\Windows\System32\WindowsPowerShell\v1.0',
             r'C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit',
             r'C:\Program Files\Microsoft SQL Server\110\Tools\Binn',
+            r'C:\Program Files (x86)\CMake\bin',
+            r'C:\Program Files\SlikSvn\bin',
+            r'C:\Program Files\Git\bin',
+            r'C:\Program Files (x86)\WinSCP',
         ])
 
         INCLUDE = (

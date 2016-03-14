826468d76c69c0b3d23a19bc85c1b4c0cbae9c55
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2016-03-14 20:32:35 +0200

    fix: incorrect hash used

--- builder/builder.py
+++ builder/builder.py
@@ -477,7 +477,7 @@ class Builder:
 		with open(buildinfoTxt, 'w') as buildinfo:
 			buildinfo.write('#define BUILD_COMMIT_TIMESTAMP 0\n')
 			buildinfo.write('#define BUILD_BRANCH "%s"\n' % (self.use_github_branch))
-			buildinfo.write('#define BUILD_HASH "%s"\n' % (self.brev))
+			buildinfo.write('#define BUILD_HASH "%s"\n' % (self.revision[:7]))
 			buildinfo.write('#define BUILD_DATE "%s"\n' % (now.strftime("%d %b %Y")))
 			buildinfo.write('#define BUILD_TIME "%s"\n' % (now.strftime("%H:%M:%S")))
 			buildinfo.write('\n')

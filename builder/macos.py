8db1d89f87c56d711d8189dbeef90a40143c2c80
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2016-08-22 13:59:04 +0300

    fix: teamcity osx building

--- builder/macos.py
+++ builder/macos.py
@@ -71,6 +71,7 @@ class MacBuilder(Builder):
 			cmake.append("-DWITH_OPENIMAGEIO=OFF")
 			cmake.append("-DWITH_IMAGE_OPENEXR=OFF")
 			cmake.append("-DWITH_FFTW3=OFF")
+			cmake.append("-DCMAKE_OSX_DEPLOYMENT_TARGET=")
 		else:
 			cmake.append("-DWITH_GAMEENGINE=%s" % utils.GetCmakeOnOff(self.with_ge))
 			cmake.append("-DWITH_PLAYER=%s" % utils.GetCmakeOnOff(self.with_player))

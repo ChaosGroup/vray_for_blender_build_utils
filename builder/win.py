e14af9c1b9cbee1a802b9e678dca2436dddc6c72
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2018-02-15 16:48:54 +0200

    fix: disable DWITH_CYCLES_CUDA

--- builder/win.py
+++ builder/win.py
@@ -123,7 +123,7 @@ class WindowsBuilder(Builder):
 			if self.with_cycles:
 				cmake.append("-DWITH_LLVM=ON")
 				cmake.append("-DWITH_CYCLES_OSL=ON")
-				cmake.append("-DWITH_CYCLES_CUDA=ON")
+				# cmake.append("-DWITH_CYCLES_CUDA=ON")
 				# cmake.append("-DWITH_CYCLES_CUDA_BINARIES=ON")
 			cmake.append("-DWITH_MOD_OCEANSIM=ON")
 			cmake.append("-DWITH_OPENSUBDIV=ON")

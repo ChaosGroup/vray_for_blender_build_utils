4b0f0b4f7e9ce7324ae2789466f94f13b2abbb53
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2018-02-15 15:59:15 +0200

    fix: numpy download url

--- builder/linux.py
+++ builder/linux.py
@@ -144,7 +144,7 @@ def getDepsCompilationData(self, prefix, wd, jobs):
 		)),
 		('numpy', '%s/python/lib/python%s/site-packages/numpy' % (prefix, PYTHON_VERSION_BIG), (
 			getChDirCmd(wd),
-			getDownloadCmd("https://freefr.dl.sourceforge.net/project/numpy/NumPy/%s/numpy-%s.tar.gz" % (NUMPY_VERSION, NUMPY_VERSION), 'numpy.tar.gz'),
+			getDownloadCmd("https://github.com/numpy/numpy/releases/download/v%s/numpy-%s.tar.gz" % (NUMPY_VERSION, NUMPY_VERSION), 'numpy.tar.gz'),
 			'tar -C . -xf numpy.tar.gz',
 			getChDirCmd(os.path.join(wd, 'numpy-%s' % NUMPY_VERSION)),
 			'%s/python/bin/python3 setup.py install --prefix=%s/numpy-%s' % (prefix, prefix, NUMPY_VERSION),

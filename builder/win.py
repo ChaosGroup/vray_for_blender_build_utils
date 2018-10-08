c7d46561b68dabfd92c3e13c2c5546999a3657c4
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2018-10-08 15:18:56 +0300

    fix: enable cycles cuda for windows

--- builder/win.py
+++ builder/win.py
@@ -164,8 +164,8 @@ class WindowsBuilder(Builder):
 			if self.with_cycles:
 				cmake.append("-DWITH_LLVM=ON")
 				cmake.append("-DWITH_CYCLES_OSL=ON")
-				# cmake.append("-DWITH_CYCLES_CUDA=ON")
-				# cmake.append("-DWITH_CYCLES_CUDA_BINARIES=ON")
+				cmake.append("-DWITH_CYCLES_CUDA=ON")
+				cmake.append("-DWITH_CYCLES_CUDA_BINARIES=ON")
 			cmake.append("-DWITH_MOD_OCEANSIM=ON")
 			cmake.append("-DWITH_OPENSUBDIV=ON")
 			cmake.append("-DWITH_FFTW3=ON")
